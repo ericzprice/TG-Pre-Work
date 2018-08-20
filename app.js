d319de1734744c199e0f1582da09ede42819fc19
ericzprice <ericzprice@gmail.com>
2018-08-20 08:59:42 -0600

    Intro to JS 1-6

---
+++ app.js
@@ -0,0 +1,37 @@
+// JS: Console
+.log("Sausage");
+console.log("Oxymoronica");
+// End JS: Console
+//
+//
+// JS: Datatypes
+console.log("JavaScript")
+console.log(33.7)
+console.log(true)
+console.log(null)
+// End JS: Datatypes
+//
+//
+// JS: Math Operators
+console.log(3.5 + 45);
+console.log(2018 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
+// End JS: Math Operators
+
+
+// JS: Properties
+console.log('Teaching the world how to code'.length);
+// End JS: Properties
+//
+//
+// JS: Built in Methods
+//
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+// End JS: Built in Methods
+//
+//
