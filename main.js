62bea552d14990f4098ab7929c5177795f81a59d
ericzprice <ericzprice@gmail.com>
2018-08-21 15:31:56 -0600

    Iterators 4-7

--- main.js
+++ main.js
@@ -1,68 +1,3 @@
-// Intro to Iterators
-let artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
-
-artists.forEach(function(artist) {
-  console.log(artist + ' is one of my favorite artists.');
-});
-
-let numbers = [1, 2, 3, 4, 5];
-
-let squareNumbers = numbers.map(function(number) {
-  return number * number;
-});
-
-console.log(squareNumbers);
-
-let things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
-
-let onlyNumbers = things.filter(function(thing) {
-  return typeof thing === 'number';
-});
-
-console.log(onlyNumbers);
-// End Intro to Iterators
-
-
-// .forEach()
-let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
-
-// Iterate over fruits below
-fruits.forEach(function(fruit) {
-  console.log(' I want to eat a ' + fruit);
-});
-
-// Refactor the .forEach() call you just wrote using arrow function syntax instead.
-fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
-// End .forEach()
-//
-//
-// .map()
-let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
-
-// Create the secretMessage array below
-
-let secretMessage = animals.map(animal => animal.charAt(0));
-
-console.log(secretMessage.join(''));
-
-let bigNumbers = [100, 200, 300, 400, 500];
-
-// Create the smallNumbers array below
-
-let smallNumbers = bigNumbers.map(function(number) {
-  return number / 100;
-});
-// Prints out...
-// HelloWorld   ...from all the 1st letters of animals
-
-// Refactor the method call from Step 1 using arrow function syntax.
-let smallNumbers = bigNumbers.map(num => num/100);
-console.log(smallNumbers);
-// Prints out...
-// [ 1, 2, 3, 4, 5 ]
-// End .map()
-
-
 // .filter()
 let randomNumbers = [375, 200, 3.14, 7, 13, 852];
 
@@ -77,8 +12,58 @@ console.log(smallNumbers);
 let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
 
 let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
-
+// Refactor the code above using arrow function syntax
 console.log(longFavoriteWords);
 // [ 'nostalgia', 'hyperbole', 'esoteric' ]
+//
+//
+// End .filter()
+//
+//
+// Iterator Documentation
+let words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+// In the code editor, there is an array called words. We want to create a new array of interesting words. The first thing we want to do is check if there are words that are fewer than 6 characters long. There is something missing in the words.some() method call. Fix this method so that true is printed to the console.
 
-// Refactor the code above using arrow function syntax
+console.log(words.some(function(word) {
+  return word.length < 6;
+}));
+// True
+// Use filter to create a new array
+
+let interestingWords = words.filter(word => word.length > 5);
+// The method returned true, which means there are some words in the array that are shorter than six characters. Use the .filter() method to save any words longer than 5 characters to a new array called interestingWords, declared with let.
+console.log(interestingWords);
+// [ 'unique', 'uncanny', 'oxymoron' ]
+//
+console.log(interestingWords.every(word =>  word.length > 5));
+// True
+// End Iterator Documentation
+//
+//
+// Choose the Right Iterator
+
+let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+
+let nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+// Have you visited Nashville?
+// Have you visited Charlotte?
+// Have you visited Asheville?
+// Have you visited Austin?
+// Have you visited Boulder?
+
+// Choose a method that will return a new array
+let longCities = cities.filter(city => city.length > 7);
+console.log(longCities);
+ // 'Nashville', 'Charlotte', 'Asheville' ]
+ //
+// Choose a method that will return a new array
+let smallerNums = nums.map(num => num - 5);
+ console.log(smallerNums);
+ // [-4, 45, 70, 195, 345, 520, 995 ]
+ //
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
+// End Choose the Right Iterator
