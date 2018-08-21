db60a740b248cf9894fb94a97818a6e237fecb3a
ericzprice <ericzprice@gmail.com>
2018-08-21 10:37:32 -0600

    Loops

--- main.js
+++ main.js
@@ -1,101 +1,100 @@
-// Arrays
+// Loops
+let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
+
+for (let i = 0; i<cookies.length; i++) {
+  console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
+}
+// I would love to eat a chocolate chip cookie right now!
+// I would love to eat a raisin cookie right now!
+// I would love to eat a macadamia nut cookie right now!
+// I would love to eat a sugar cookie right now!
 //
-let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
-
-console.log(bucketList);
-
-// Create an Array
-let newYearsResolutions = ['Climb a new wall', 'Skateboard at a old park', 'Play my guitar'];
-
-console.log(newYearsResolutions);
-// End Create an Array
-//
-//
-// Property Access
-let newYearsResolutions = ['Climb a new wall', 'Skateboard at a old park', 'Play my guitar'];
-
-let listItem = newYearsResolutions[0];
-console.log(listItem);
-
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions[3]);
-// Try to log the item at position [3] to the console. What is logged to the console?
-// 'Play my guitar' in [2] position
-
-// End Property Access
+// End Loops
 //
 //
-// Update Elements
-let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+// Looping Manually
 
-newYearsResolutions[1] = "Learn a new language";
-console.log(newYearsResolutions)
-// End Update Elements
+let vacationSpots = ['Paris', 'Italy', 'Porto'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+for (let i = 0; i<vacationSpots.length; i++) {
+  console.log('I would love to visit ' + vacationSpots[i] + ' soon!');
+
+// End Looping Manually
 //
 //
-// Length Property
-let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+// for Loops
+let vacationSpots = ['Paris', 'Italy', 'Porto'];
 
-console.log(newYearsResolutions.length);
-// End Length Property
+for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++) {
+  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
+// End for Loops
 //
 //
-// push Method
-let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+// for Loops, Backwards
+let vacationSpots = ['Paris', 'Italy', 'Porto'];
 
-newYearsResolutions.push('Learn to play the harmonica', 'Give a speech');
-
-console.log(newYearsResolutions);
-
-newYearsResolutions.pop();
-console.log(newYearsResolutions);
-// End push Method
+for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; vacationSpotIndex--) {
+  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
+// Because arrays start counting at 0, the start condition needs to be the length of the vacationSpots array minus 1.
 //
+// End for Loops, Backwards
+
+
+// Nested for Loops!!!
+let myPlaces = ['Kansas', 'Taos', 'Springfield'];
+
+let friendPlaces = ['Taos', 'Charlotte', 'Asheville'];
+
+for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
+  for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
+		if (myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
+      console.log(friendPlaces[friendPlacesIndex]);
+    }
+  }
+}
+// 5.
+// Look what printed to the console. Your first place printed, then all three of your friend's. Then your second place, then your friend's places again. Finally, your third place printed and all three of your friend's places printed a final time.
 //
-// More Array Methods
-let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
-
-groceryList.shift();
-// Use the .shift() method to remove the first item from the array groceryList.
-
-groceryList.unshift('popcorn');
-console.log(groceryList);
-// Use the .unshift() method to add 'popcorn' to the beginning of your grocery list.
-
-console.log(groceryList.slice(1, 4));
-// You want him to pick up the bananas, coffee beans, and brown rice.
-// Use .slice() to provide him with a list of these three things.
-
-console.log(groceryList);
-// Log the grocery list to the console one more time.
-// Notice that the groceryList array still contains the same items it had in Step 2.
-
-// End More Array Methods
-
-
-// Arrays with let and const
-let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
-
-const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
-
-condiments.push('Pesto');
-
-console.log(condiments);
-
-condiments = ['Soy Sauce'];
-
-console.log(condiments);
-
-utensils.pop();
-// Below your code from Step 2, remove the last item from the utensils array using .pop().
-console.log(utensils);
-
-console.log(condiments);
-
-utensils = [ 'Fork'];
-// Below your code from Step 3, reassign the utensils array to be a new array with only your favorite utensil inside of it.
-// You should have received an error!
+// This is because the inner for loop runs completely every time the outer for loop runs once!!!!
 //
-// End Arrays with let and const
+// The purpose of the program is to see what you and your friend have in common. Let's utilize the === comparison with an if statement.
 //
+// Delete the two console.log() statements you wrote in the previous steps.
 //
+// Inside the second for loop's block, write an if statement that checks if myPlaces[myPlacesIndex] is equal to friendPlaces[friendPlacesIndex]. If it is, log to the console the place you have in common.
+// End Nested for Loops
+
+
+// while Loops
+let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+
+let currentCard = 'Heart';
+
+while (currentCard !== 'Spade') {
+  console.log(currentCard);
+  currentCard = cards[Math.floor(Math.random() *4)];
+  // This code will generate a random number between 0 and 3, the range of indices of the cards array, and reassign currentCard to a new card from that array. Because the while loop only runs if the card is NOT a Spade, the value of currentCard will only be logged to the console if it is not 'Spade'.
+}
+
+console.log('You found a spade!');
+// End while Loops
+
+
+// Infinite Loops
+let flag = true;
+let counter = 0;
+while(flag === true){
+  console.log(counter);
+  counter+=1;
+  if (counter === 37){
+    break;
+  }
+}
+// This code will terminate the while loop, with the break keyword, when the condition counter === 37 is met. Without this code the while loop would run infinitely because there is no condition stated to "break" the loop.
+// End Infinite Loops
