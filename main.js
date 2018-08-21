a8e000191891cbab533cdbe4baa3a077db5a9820
ericzprice <ericzprice@gmail.com>
2018-08-21 14:04:25 -0600

    Iterators 1-3

--- main.js
+++ main.js
@@ -1,100 +1,84 @@
-// Loops
-let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
+// Intro to Iterators
+let artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
 
-for (let i = 0; i<cookies.length; i++) {
-  console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
-}
-// I would love to eat a chocolate chip cookie right now!
-// I would love to eat a raisin cookie right now!
-// I would love to eat a macadamia nut cookie right now!
-// I would love to eat a sugar cookie right now!
+artists.forEach(function(artist) {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+let numbers = [1, 2, 3, 4, 5];
+
+let squareNumbers = numbers.map(function(number) {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+let things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+let onlyNumbers = things.filter(function(thing) {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+// End Intro to Iterators
+
+
+// .forEach()
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(function(fruit) {
+  console.log(' I want to eat a ' + fruit);
+});
+
+// Refactor the .forEach() call you just wrote using arrow function syntax instead.
+fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+// End .forEach()
 //
-// End Loops
 //
-//
-// Looping Manually
+// .map()
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-let vacationSpots = ['Paris', 'Italy', 'Porto'];
+// Create the secretMessage array below
 
-console.log(vacationSpots[0]);
-console.log(vacationSpots[1]);
-console.log(vacationSpots[2]);
+let secretMessage = animals.map(animal => animal.charAt(0));
 
-for (let i = 0; i<vacationSpots.length; i++) {
-  console.log('I would love to visit ' + vacationSpots[i] + ' soon!');
+console.log(secretMessage.join(''));
 
-// End Looping Manually
-//
-//
-// for Loops
-let vacationSpots = ['Paris', 'Italy', 'Porto'];
+let bigNumbers = [100, 200, 300, 400, 500];
 
-for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++) {
-  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
-}
-// End for Loops
-//
-//
-// for Loops, Backwards
-let vacationSpots = ['Paris', 'Italy', 'Porto'];
+// Create the smallNumbers array below
 
-for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; vacationSpotIndex--) {
-  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
-}
-// Because arrays start counting at 0, the start condition needs to be the length of the vacationSpots array minus 1.
-//
-// End for Loops, Backwards
+let smallNumbers = bigNumbers.map(function(number) {
+  return number / 100;
+});
+// Prints out...
+// HelloWorld   ...from all the 1st letters of animals
+
+// Refactor the method call from Step 1 using arrow function syntax.
+let smallNumbers = bigNumbers.map(num => num/100);
+console.log(smallNumbers);
+// Prints out...
+// [ 1, 2, 3, 4, 5 ]
+// End .map()
 
 
-// Nested for Loops!!!
-let myPlaces = ['Kansas', 'Taos', 'Springfield'];
+// .filter()
+let randomNumbers = [375, 200, 3.14, 7, 13, 852];
 
-let friendPlaces = ['Taos', 'Charlotte', 'Asheville'];
+// Call .filter() on randomNumbers below
 
-for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
-  for (let friendPlacesIndex = 0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++) {
-		if (myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]) {
-      console.log(friendPlaces[friendPlacesIndex]);
-    }
-  }
-}
-// 5.
-// Look what printed to the console. Your first place printed, then all three of your friend's. Then your second place, then your friend's places again. Finally, your third place printed and all three of your friend's places printed a final time.
-//
-// This is because the inner for loop runs completely every time the outer for loop runs once!!!!
-//
-// The purpose of the program is to see what you and your friend have in common. Let's utilize the === comparison with an if statement.
-//
-// Delete the two console.log() statements you wrote in the previous steps.
-//
-// Inside the second for loop's block, write an if statement that checks if myPlaces[myPlacesIndex] is equal to friendPlaces[friendPlacesIndex]. If it is, log to the console the place you have in common.
-// End Nested for Loops
+let smallNumbers = randomNumbers.filter(num => num < 250);
 
+console.log(smallNumbers);
+// [ 200, 3.14, 7, 13 ]
 
-// while Loops
-let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+// 2.
+let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
 
-let currentCard = 'Heart';
+let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
 
-while (currentCard !== 'Spade') {
-  console.log(currentCard);
-  currentCard = cards[Math.floor(Math.random() *4)];
-  // This code will generate a random number between 0 and 3, the range of indices of the cards array, and reassign currentCard to a new card from that array. Because the while loop only runs if the card is NOT a Spade, the value of currentCard will only be logged to the console if it is not 'Spade'.
-}
+console.log(longFavoriteWords);
+// [ 'nostalgia', 'hyperbole', 'esoteric' ]
 
-console.log('You found a spade!');
-// End while Loops
-
-
-// Infinite Loops
-let flag = true;
-let counter = 0;
-while(flag === true){
-  console.log(counter);
-  counter+=1;
-  if (counter === 37){
-    break;
-  }
-}
-// This code will terminate the while loop, with the break keyword, when the condition counter === 37 is met. Without this code the while loop would run infinitely because there is no condition stated to "break" the loop.
-// End Infinite Loops
+// Refactor the code above using arrow function syntax
