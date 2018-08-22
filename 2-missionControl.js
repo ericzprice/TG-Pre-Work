0d597253055d3fbe086675ba76117c8575a5b4e9
ericzprice <ericzprice@gmail.com>
2018-08-22 14:59:23 -0600

    INTERMEDIATE JAVASCRIPT MODULES Import as

--- 2-missionControl.js
+++ 2-missionControl.js
@@ -1,31 +1,29 @@
-// Import Named Imports --NEEDS STUDY
+// Import As
 
-import {availableAirplanes, flightRequirements, meetsStaffRequirements, meetsSpeedRangeRequirements} from './airplane';
+import {aircrafts, flightReqs, meetsStaffReqs, meetsSpeedRangeReqs} from './airplane';
 
 function displayFuelCapacity() {
   availableAirplanes.forEach(function(element) {
-    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+    console.log('Fuel Capacity of ' + element['name'] + ': ' + element['fuelCapacity']);
   });
 }
 
 displayFuelCapacity();
 
 function displayStaffStatus() {
-  availableAirplanes.forEach(function(element) {
-   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  aircrafts.forEach(function(element) {
+   console.log(element['name'] + ' meets staff requirements: ' + meetsStaffReqss(element['availableStaff'], flightRequirements['requiredStaff']) );
   });
 }
 
 displayStaffStatus();
 
 function displaySpeedRangeStatus() {
-      // Within the displaySpeedRangeStatus() function, use the forEach method to iterate over each element in the availableAirplanes array.
-      // Again, the forEach() should take a function as a parameter; this function should take element as a parameter.
-  availableAirplanes.forEach(function(element) {
-   console.log(element.name + ' meets speed range requirements:' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+  aircrafts.forEach(function(element) {
+   console.log(element.name' + ' meets speed range requirements: ' + ' meetsSpeedRangeReqs(element.maxSpeed, element.minSpeed, flightReqs.requiredSpeedRange));
   });
 }
 
 displaySpeedRangeStatus();
 
-// End Import Named Imports
+// End Import As
