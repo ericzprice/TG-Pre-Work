d10b09ae3664a12bc5c0290350944b71d143edc5
ericzprice <ericzprice@gmail.com>
2018-08-22 14:02:15 -0600

    INTERMEDIATE JAVASCRIPT MODULES Import

--- 2-airplane.js
+++ 2-airplane.js
@@ -1,10 +1,19 @@
-// module.exports II
+// export default
 
-const Airplane = {};
+let Airplane = {};
+// Now that we have an object Airplane, we can continue by adding data in the form of properties and values to the Airplane module.
+// Create an availableAirplanes variable and set it equal to an empty array. Be sure that availableAirplanes is a property of the Airplane object.
+Airplane.availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800
+ },
+ {
+   name: 'SkyJet',
+  fuelCapacity: 500
+ }
+];
 
-module.exports = {
-  myAirplane: "CloudJet",
-  displayAirplane: function() {
-    return this.myAirplane;
-  }
-};
+export default Airplane;
+
+// End export default
--- 2-missionControl.js
+++ 2-missionControl.js
@@ -1,3 +1,17 @@
-// module.exports II
-const Airplane = require('./2-airplane.js');
-console.log(Airplane.displayAirplane());
+// Import
+
+import Airplane from './airplane';
+
+// Within the body of the displayFuelCapacity function, use forEach() to iterate over the Airplane.availableAirplanes array.
+// The forEach() should take an anonymous function as a parameter. We'll add more in the next step.
+function displayFuelCapacity() {
+  Airplane.availableAirplanes.forEach(function(element){
+        // Pass the anonymous function you created in the last step a parameter of element.
+  console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  // Within the displayFuelCapacity function, use console.log() to output the element's name and its fuel capacity.
+  });
+}
+
+displayFuelCapacity();
+
+// End Import
