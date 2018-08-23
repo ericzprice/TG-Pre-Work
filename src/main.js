a9fbfb32fef5c77c88fbf60a136ca74217a34bc1
ericzprice <ericzprice@gmail.com>
2018-08-23 16:21:45 -0600

    Requests I #5-

--- src/main.js
+++ src/main.js
@@ -1,84 +1,154 @@
-Requests
 
-// Intro to Requests
-const jsonButton = document.querySelector('#generate');
-const buttonContainer = document.querySelector('#buttonContainer');
-const display = document.querySelector('#displayContainer');
-const collection = ["Another", "More", "Next", "Continue", "Keep going", "Click me", "A new one"];
+// XHR GET Requests III
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_rhy=';
 
-const generateJson = () => {
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+
   const xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-  renderResponse(xhr.response);
-      changeButton();
+      renderResponse(xhr.response);
     }
   }
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+
+  xhr.open('GET', endpoint);
   xhr.send();
 }
 
-const formatJson = (resJson) => {
-  resJson = JSON.stringify(resJson);
-  let counter = 0;
-  return resJson.split('')
-  .map(char => {
-    switch (char) {
-      case ',':
-        return `,\n${' '.repeat(counter * 2)}`;
-      case '{':
-        counter += 1;
-        return `{\n${' '.repeat(counter * 2)}`;
-      case '}':
-        counter -= 1;
-        return `\n${' '.repeat(counter * 2)}}`;
-      default:
-        return char;
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
+
+// End XHR GET Requests III
+//
+//
+// XHR GET Requests IV
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jjb=';
+const additionalParams = '&topics=';
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const topicField = document.querySelector('#topic');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const topicQuery = topicField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}${additionalParams}${topicQuery}`;
+
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderResponse(xhr.response);
     }
-  })
-  .join('');
+  }
+
+  xhr.open('GET', endpoint);
+  xhr.send();
 }
 
-const renderResponse = (jsonResponse) => {
-  const jsonSelection = Math.floor(Math.random() * 10);
-  display.innerHTML = `<pre>${formatJson(jsonResponse[jsonSelection])}</pre>`;
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
 }
 
-const changeButton = () => {
-  const newText = Math.floor(Math.random() * 7);
-  jsonButton.innerHTML = `${collection[newText]}!`;
-}
+submit.addEventListener('click', displaySuggestions);
 
-jsonButton.addEventListener('click', generateJson);
-// End Intro to Requests
+// End XHR GET Requests IV
 //
 //
-// HTTP Requests
-console.log('First message!');
-setTimeout(() => {
-   console.log('This message will always run last...');
-}, 0);
-console.log('Second message!');
-// What if we change the 2500 in setTimeout() to be 0? Essentially the callback doesn't need to wait before it can be called. Do you think that this change will affect the order? No, it didn't change things.
-// End HTTP Requests
-//
-//
-// XHR GET Requests II
+// XHR POST Requests I and II
 const xhr = new XMLHttpRequest();
 const url = 'https://api-to-call.com/endpoint';
+const data = JSON.stringify({id: '200'});
+
 xhr.responseType = 'json';
+
 xhr.onreadystatechange = () => {
-  if (xhr.readyState === XMLHttpRequest.DONE){
-        // The purpose of this conditional statement checks to see if the request has finished.
+  if(xhr.readyState === XMLHttpRequest.DONE){
     return xhr.response;
   }
 }
 
-xhr.open('GET', url);
-xhr.send();
+xhr.open('POST', url);
+xhr.send(data);
+// End XHR POST Requests I and II
+//
+//
+//
+// XHR POST Requests III
+// Information to reach API
+const apiKey = '19188808d08943bc9e73b97e46f274f0';
+const url = 'https://api.rebrandly.com/v1/links';
 
-// End XHR GET Requests II
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+  		renderResponse(xhr.response);
+		}
+  }
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.setRequestHeader('apikey', apiKey);
+  xhr.send(data);
+}
+
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+// Your shortened url is now:
+// rebrand.ly/14hgo
+// and it use to be https://medium.com/@codecademy/breaking-the-coding-language-barrier-bf24652c3c60
 //
-//
+// End XHR POST Requests III
