bf92d9e4979ac19d644598d6eb331554586b0575
ericzprice <ericzprice@gmail.com>
2018-08-24 10:15:43 -0600

    Requests II #1-10

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
+// Requests II:
+// fetch() GET Requests II
 
-// XHR GET Requests III
-// Information to reach API
-const url = 'https://api.datamuse.com/words?';
-const queryParams = 'rel_rhy=';
+fetch('https://api-to-call.com/endpoint').then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message);
+}).then(jsonResponse => {
+  return jsonResponse;
+});
+//
+// End fetch() GET Requests II
+//
+//
+// fetch() GET Requests III
+Information to reach API
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
 
-// Selecting page elements
+// Selects page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
@@ -14,101 +30,100 @@ const getSuggestions = () => {
   const wordQuery = inputField.value;
   const endpoint = `${url}${queryParams}${wordQuery}`;
 
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderResponse(xhr.response);
+  fetch(endpoint).then(response => {
+    if (response.ok) {
+      return response.json();
     }
-  }
-
-  xhr.open('GET', endpoint);
-  xhr.send();
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  })
 }
 
-// Clear previous results and display results to webpage
+// Clears previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild);
   }
   getSuggestions();
-}
+};
 
 submit.addEventListener('click', displaySuggestions);
 
-
-// End XHR GET Requests III
+// End fetch() GET Requests III
 //
 //
-// XHR GET Requests IV
+// fetch() GET Requests IV
 // Information to reach API
-const url = 'https://api.datamuse.com/words?';
-const queryParams = 'rel_jjb=';
-const additionalParams = '&topics=';
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
 
-// Selecting page elements
+// Selects page elements
 const inputField = document.querySelector('#input');
-const topicField = document.querySelector('#topic');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
 // AJAX function
 const getSuggestions = () => {
   const wordQuery = inputField.value;
-  const topicQuery = topicField.value;
-  const endpoint = `${url}${queryParams}${wordQuery}${additionalParams}${topicQuery}`;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
 
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderResponse(xhr.response);
+  fetch(endpoint).then(response => {
+    if (response.ok) {
+      return response.json();
     }
-  }
-
-  xhr.open('GET', endpoint);
-  xhr.send();
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  })
 }
-
-// Clear previous results and display results to webpage
+// You can view the purpose of renderRawResponse at public/helperFunctions.js.
+//
+//
+//
+// Clears previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild);
   }
   getSuggestions();
-}
+};
 
 submit.addEventListener('click', displaySuggestions);
-
-// End XHR GET Requests IV
+// End fetch() GET Requests IV
 //
 //
-// XHR POST Requests I and II
-const xhr = new XMLHttpRequest();
-const url = 'https://api-to-call.com/endpoint';
-const data = JSON.stringify({id: '200'});
-
-xhr.responseType = 'json';
-
-xhr.onreadystatechange = () => {
-  if(xhr.readyState === XMLHttpRequest.DONE){
-    return xhr.response;
+// fetch() POST Requests II
+//
+// It has the boilerplate code for a POST request using fetch().
+// Notice that the initial call takes two arguments: an endpoint and an object that contains information needed for the POST request. The rest of the request is identical to the GET request.
+//
+// https://s3.amazonaws.com/codecademy-content/courses/intermediate-javascript-requests/diagrams/fetch+POST+diagram.svg
+//
+fetch('https://api-to-call.com/endpoint', {
+  method: 'POST',
+  body: JSON.stringify({id: "200"})
+}).then(response => {
+  if(response.ok){
+	  return response.json();
   }
-}
-
-xhr.open('POST', url);
-xhr.send(data);
-// End XHR POST Requests I and II
+	throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message);
+}).then(jsonResponse => {
+  console.log(jsonResponse);
+})
+// End fetch() POST Requests II
 //
 //
+// fetch() POST Requests III
 //
-// XHR POST Requests III
-// Information to reach API
-const apiKey = '19188808d08943bc9e73b97e46f274f0';
+//// Information to reach API
+const apiKey = '<19188808d08943bc9e73b97e46f274f0>';
 const url = 'https://api.rebrandly.com/v1/links';
 
 // Some page elements
@@ -119,36 +134,235 @@ const responseField = document.querySelector('#responseField');
 // AJAX functions
 const shortenUrl = () => {
   const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten});
+  const data = JSON.stringify({destination: urlToShorten})
 
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-  		renderResponse(xhr.response);
-		}
-  }
-  xhr.open('POST', url);
-  xhr.setRequestHeader('Content-type', 'application/json');
-	xhr.setRequestHeader('apikey', apiKey);
-  xhr.send(data);
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  })
 }
-
 
 // Clear page and call AJAX functions
 const displayShortUrl = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+//
+//
+//                       Helper Functions Provided
+// Manipulates responseField to render a formatted and appropriate message
+const renderResponse = (res) => {
+  // Displays either message depending on results
+  if(res.errors){
+    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+  } else {
+    responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`;
+  }
+}
+
+// Manipulates responseField to render an unformatted response
+const renderRawResponse = (res) => {
+  // Displays either message depending on results
+  if(res.errors){
+    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+  } else {
+    // Adds line breaks for JSON
+    let structuredRes = JSON.stringify(res).replace(/,/g, ", \n");
+    structuredRes = `<pre>${structuredRes}</pre>`;
+    responseField.innerHTML = `${structuredRes}`;
+  }
+}
+
+// Renders the JSON that was returned when the Promise from fetch resolves.
+const renderJsonResponse = (response) => {
+  // Creates an empty object to store the JSON in key-value pairs
+  let rawJson = {}
+  for(let key in response){
+    rawJson[key] = response[key]
+  }
+  // Converts JSON into a string and adding line breaks to make it easier to read
+  rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n")
+  // Manipulates responseField to show the returned JSON.
+  responseField.innerHTML = `<pre>${rawJson}</pre>`
+}
+// End fetch() POST Requests III
+//
+//
+// fetch() POST Requests IV
+//
+//// Information to reach API
+const apiKey = '<19188808d08943bc9e73b97e46f274f0>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten})
+
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);//
+//
+//
+//                                    Helper Functions Provided
+
+// Manipulates responseField to render a formatted and appropriate message
+const renderResponse = (res) => {
+  // Displays either message depending on results
+  if(res.errors){
+    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+  } else {
+    responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`;
+  }
+}
+
+// Manipulates responseField to render an unformatted response
+const renderRawResponse = (res) => {
+  // Displays either message depending on results
+  if(res.errors){
+    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+  } else {
+    // Adds line breaks for JSON
+    let structuredRes = JSON.stringify(res).replace(/,/g, ", \n");
+    structuredRes = `<pre>${structuredRes}</pre>`;
+    responseField.innerHTML = `${structuredRes}`;
+  }
+}
+
+// Renders the JSON that was returned when the Promise from fetch resolves.
+const renderJsonResponse = (response) => {
+  // Creates an empty object to store the JSON in key-value pairs
+  let rawJson = {}
+  for(let key in response){
+    rawJson[key] = response[key]
+  }
+  // Converts JSON into a string and adding line breaks to make it easier to read
+  rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n")
+  // Manipulates responseField to show the returned JSON.
+  responseField.innerHTML = `<pre>${rawJson}</pre>`
+}
+// End fetch() POST Requests IV
+//
+//
+// fetch() POST Requests V
+//
+//// Information to reach API
+const apiKey = '<19188808d08943bc9e73b97e46f274f0>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten})
+
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
   }
   shortenUrl();
 }
 
 shortenButton.addEventListener('click', displayShortUrl);
 
-// Your shortened url is now:
-// rebrand.ly/14hgo
-// and it use to be https://medium.com/@codecademy/breaking-the-coding-language-barrier-bf24652c3c60
-//
-// End XHR POST Requests III
+//                       Helper Functions Provided
+//// Manipulates responseField to render a formatted and appropriate message
+const renderResponse = (res) => {
+  // Displays either message depending on results
+  if(res.errors){
+    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+  } else {
+    responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`;
+  }
+}
+
+// Manipulates responseField to render an unformatted response
+const renderRawResponse = (res) => {
+  // Displays either message depending on results
+  if(res.errors){
+    responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+  } else {
+    // Adds line breaks for JSON
+    let structuredRes = JSON.stringify(res).replace(/,/g, ", \n");
+    structuredRes = `<pre>${structuredRes}</pre>`;
+    responseField.innerHTML = `${structuredRes}`;
+  }
+}
+
+// Renders the JSON that was returned when the Promise from fetch resolves.
+const renderJsonResponse = (response) => {
+  // Creates an empty object to store the JSON in key-value pairs
+  let rawJson = {}
+  for(let key in response){
+    rawJson[key] = response[key]
+  }
+  // Converts JSON into a string and adding line breaks to make it easier to read
+  rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n")
+  // Manipulates responseField to show the returned JSON.
+  responseField.innerHTML = `<pre>${rawJson}</pre>`
+}
+
+// End fetch() POST Requests V
