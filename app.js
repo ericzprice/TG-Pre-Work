2b11ffa3bc8928ecdc6c8f190aa9f72d51edbcd2
ericzprice <ericzprice@gmail.com>
2018-08-31 10:24:13 -0600

    HIGHER-ORDER FUNCTIONS

--- app.js
+++ app.js
@@ -1,167 +1,52 @@
-// Intro to Functions
-let calculatorIsOn = false;
+// HIGHER-ORDER FUNCTIONS: Functions as Data
 
-const pressPowerButton = () => {
-//     () =>, indicating the variable stores a function. This syntax is known as arrow function syntax. In JavaScript, any code between curly braces is also known as a block.
- if (calculatorIsOn) {
-   console.log('Calculator turning off.');
-   calculatorIsOn = false;
- } else {
-   console.log('Calculator turning on.');
-   calculatorIsOn = true;
- }
-};
-
-pressPowerButton();
-// Output: Calculator turning on.
-
-pressPowerButton();
-// Output: Calculator turning off.
-//
-//End Intro to Functions
-
-
-// Functions
-const takeOrder = () => {
-  console.log('Order: pizza')
-};
-
-takeOrder();
-
-// End Functions
-//
-//
-// Parameters 1 and 2
-const takeOrder = (topping) => {
-  console.log('Order: pizza topped with ' + topping);
-};
-
-takeOrder('onions');
-
-
-// Part 2
-const takeOrder = (topping, crustType) => {
-  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-};
-
-takeOrder('bacon', 'tossed crust');
-takeOrder('garlic', 'thin crust');
-takeOrder('sausage', 'stuffed crust');
-// Browser:
-// Order: tossed crust pizza topped with bacon
-// Order: thin crust pizza topped with garlic
-// Order: stuffed crust pizza topped with sausage
-
-// End Parameters
-//
-//
-// Return 1 and 2
-//
-// let orderCount = 0;
-
-const takeOrder = (topping, crustType) => {
-  orderCount++;
-  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-};
-
-takeOrder('bacon', 'tossed crust');
-takeOrder('garlic', 'thin crust');
-takeOrder('sausage', 'stuffed crust');
-
-const getSubTotal = (itemCount) => {
-  return itemCount * 7.5;
-};
-// itemCount rather than orderCount Not sure why???
-
-console.log(getSubTotal(orderCount));
-
-
-// Return 2
-//
-// let orderCount = 0;
-
-const takeOrder = (topping, crustType) => {
-  orderCount++;
-  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-};
-
-takeOrder('bacon', 'tossed crust');
-takeOrder('garlic', 'thin crust');
-takeOrder('sausage', 'stuffed crust');
-
-const getSubTotal = (itemCount) => {
-  return itemCount * 7.5;
-};
-// Inside the getTax() function, call your getSubTotal() function and pass it the argument itemCount to get the subtotal, and then multiply the returned value by 6% (0.06). Make sure to return the result of this operation.
-const getTax = (itemCount) => {
-  return getSubTotal(itemCount) * 0.06
-}
-
-const getTotal = (itemCount) => {
-  return getTax(itemCount) + getSubTotal(itemCount)
-}
-
-console.log(getSubTotal(orderCount));
-
-console.log(getTotal(orderCount))
-
-// End Return 1 and 2
-//
-//
-// Function Declarations
-function isGreaterThan(numberOne, numberTwo) {
-  if (numberOne > numberTwo) {
-    return true;
-  } else {
-    return false;
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
   }
-}
+};
 
-isGreaterThan(7, 5)
+// Write your code below
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+is2p2();
+console.log(is2p2.name);
 
-// End Function Declarations
-
-
-// Function Expressions
+// End HIGHER-ORDER FUNCTIONS: Functions as Data
 //
-// Refactor the function declaration to be a function expression using arrow function syntax.
-// Be sure to call the function at the end.
-//
-// function isGreaterThan(numberOne, numberTwo){
-//   if(numberOne > numberTwo){
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// isGreaterThan(4, 8)
-//
-const isGreaterThan = (numberOne, numberTwo) => {
-  if(numberOne > numberTwo){
-    return true;
-  } else {
-    return false;
+// Functions as Parameters
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
   }
-}
+};
 
-isGreaterThan(4, 8)
+const addTwo = num => num + 2;
 
-// End Function Expressions
+const timeFuncRuntime = funcParameter => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now();
+  return t2 - t1;
+};
 
+// Write your code below
 
-// Arrow Functions
+const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
 
-// Refactor the volumeOfSphere function
-//
-// const volumeOfSphere = (diameter) => {
-//   return (1/6) * Math.PI * diameter * diameter * diameter;
-// };
-//
-// console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
-//
-const volumeOfSphere = diameter => (1/6) * Math.PI * diameter * diameter * diameter;
+const checkConsistentOutput = (func, val) => {
+    let firstTry = func(val);
+    let secondTry = func(val);
+    if (firstTry === secondTry) {
+        return firstTry
+    } else {
+        return 'This function returned inconsistent results'
+    }
+};
 
+checkConsistentOutput(addTwo, 10);
 
-console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
-
-// End Arrow Functions
+// End Functions as Parameters
